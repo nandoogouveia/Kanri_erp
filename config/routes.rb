6d1e6ce78e715cca6f1e70702669f0812b1dd504
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-11-27 17:09:21 +0000

    Gerado controller da Dashboard e alterado rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  #get 'dashboard/index'
   devise_for :gestors
   devise_for :admins
+  
+  root to: 'dashboard#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
