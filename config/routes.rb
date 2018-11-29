24ee6efec1513d2633905adc7a577f88ad49b788
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-11-29 03:00:52 +0000

    teste1

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :admins
 
   
-  root to: 'dashboard#index'
+  root to: 'index#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
