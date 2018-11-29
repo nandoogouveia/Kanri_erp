0dbb8fce0dcf21b76faaf8cda1e9275762c78af6
Fernando Gouveia <gouveiaramos.me@gmail.com>
2018-11-29 20:08:17 +0000

    Configurado rotas e layout da tela inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
   get 'cronograma/cronograma'
+  get 'cronograma', to: 'cronograma#cronograma'
+  
   get 'painel/painel'
+  get 'painel', to: 'painel#painel'
+  
   get 'calendario/calendario'
+  get 'calendario', to: 'calendario#calendario'
+  
   get 'chamado/chamado'
+  get 'chamado', to: "chamado#chamado"
+  
   get 'financeiro/financeiro'
+  get 'financeiro', to: 'financeiro#financeiro'
+  
   get 'index/index'
-  #get 'dashboard/index'
+  get 'inicio', to: 'index#index'
+
   devise_for :gestors
   devise_for :admins
 
   
-  root to: 'index#index'
+ root to: 'index#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
